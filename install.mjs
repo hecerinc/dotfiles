f93bf558005dad6a8cca0cedb0a378989f3af9a9
Hector Rincon <hecerinc@hotmail.com>
2023-06-22 21:37:49 -0700

    Add correct functions for fs. Add spaceship prompt

--- install.mjs
+++ install.mjs
@@ -3,74 +3,81 @@
 const homeDir = os.homedir();
 
 // vim-plug
-// -------------------------------------------------- 
+// --------------------------------------------------
 console.log(chalk.blue("Checking if vim-plug exists..."));
-if (fs.exists(`${homeDir}/.vim/autoload/plug.vim`)) {
-  console.log(chalk.green("You already have vim-plug, awesome!"));
+if (fs.existsSync(`${homeDir}/.vim/autoload/plug.vim`)) {
+	console.log(chalk.green("You already have vim-plug, awesome!"));
 } else {
-  console.log(chalk.red("Nope, installing vim-plug"));
+	console.log(chalk.red("Nope, installing vim-plug"));
 
-  await $`curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+	await $`curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim`;
 }
 
 // Ripgrep
-// -------------------------------------------------- 
+// --------------------------------------------------
 console.log(chalk.blue("Checking if rg exists..."));
 try {
-  await $`which rg`;
-  console.log(chalk.green("You already have rg, awesome!"));
+	await $`which rg`;
+	console.log(chalk.green("You already have rg, awesome!"));
 } catch {
-  console.log(chalk.red("Nope, installing rg (rigrep)"));
+	console.log(chalk.red("Nope, installing rg (rigrep)"));
 
-  // Debian-based stuff
-  await $`sudo apt install -y ripgrep`;
+	// Debian-based stuff
+	await $`sudo apt install -y ripgrep`;
 }
 
 // ZSH
-// -------------------------------------------------- 
+// --------------------------------------------------
 console.log(chalk.blue("Checking if zsh is installed"));
 try {
-  await $`which zsh`;
-  console.log(chalk.green("You already have zsh, awesome!"));
+	await $`which zsh`;
+	console.log(chalk.green("You already have zsh, awesome!"));
 } catch (p) {
-  console.log(chalk.red("Nope, installing zsh"));
+	console.log(chalk.red("Nope, installing zsh"));
 
-  await $`sudo apt install -y zsh`;
+	await $`sudo apt install -y zsh`;
 }
 
 // Oh-my-zsh
-// -------------------------------------------------- 
+// --------------------------------------------------
 console.log(chalk.blue("Checking if oh-my-zsh is installed"));
-if (fs.exists(`${homeDir}/.oh-my-zsh/themes/README.md`)) {
-  console.log(chalk.green("You already have oh-my-zsh, awesome!"));
+if (fs.existsSync(`${homeDir}/.oh-my-zsh/themes/README.md`)) {
+	console.log(chalk.green("You already have oh-my-zsh, awesome!"));
 } else {
-  console.log(chalk.red("Nope, installing oh-my-zsh"));
+	console.log(chalk.red("Nope, installing oh-my-zsh"));
 
-  await $`sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"`;
+	await $`sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`;
+
+	// spaceship-prompt
+	// --------------------------------------------------
+	await $`git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1`;
+	await $`ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"`;
+
+	await $`sed -i s/robbyrussell/spaceship/ ~/.zshrc`;
 }
 
 // .vimrc
-// -------------------------------------------------- 
+// --------------------------------------------------
 console.log(chalk.blue("Copying .vimrc to ~/.vimrc"));
 await nothrow($`cp -i .vimrc ~/.vimrc`);
 
 // .gitconfig
-// -------------------------------------------------- 
+// --------------------------------------------------
 console.log(chalk.blue("Copying .gitconfig to ~/.gitconfig"));
 await nothrow($`cp -i .gitconfig ~/.gitconfig`);
 
 console.log(chalk.blue("Copying .zsh-aliases to ~/.zsh-aliases"));
 await nothrow($`cp -i .zsh-aliases ~/.zsh-aliases`);
 
-fs.readFile(`${homeDir}/.zshrc`, async (err, data) => {
-  if (err) throw err;
-  if (data.includes("zsh-aliases")) {
-    console.log(
-      chalk.green("~/.zshrc contains .zsh-alises so I'm not adding it")
-    );
-  } else {
-    console.log(chalk.red("Adding .zsh-aliases to ~/.zshrc"));
-    await $`echo "source ~/.zsh-aliases" >> ~/.zshrc`;
-  }
+fs.readFileSync(`${homeDir}/.zshrc`, async (err, data) => {
+	if (err) throw err;
+	if (data.includes("zsh-aliases")) {
+		console.log(
+			chalk.green("~/.zshrc contains .zsh-alises so I'm not adding it")
+		);
+	} else {
+		console.log(chalk.red("Adding .zsh-aliases to ~/.zshrc"));
+		await $`echo "source ~/.zsh-aliases" >> ~/.zshrc`;
+	}
 });
